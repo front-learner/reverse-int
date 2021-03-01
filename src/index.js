8b785def43427f353521c67f85401cf402cc0b0b
front-learner <79720458+front-learner@users.noreply.github.com>
2021-03-02 00:06:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function reverse (n) {
-  if (n < 0) {
-    n = Math.abs(n);
+  n = Math.abs(n);
   let result = '';
   let str = String(n);
   for (let i = 0; i < str.length; i++) {
